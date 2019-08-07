8dd5e5c90d704143cdbea28a7a72b28163f0c23d
somewheve <somewheve@gmail.com>
2019-08-08 03:58:31 +0800

    init

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from engine import AsyncEngine, Event
+from async_event import AsyncEngine, Event
 
 if __name__ == '__main__':
     # easy use example
 
     EVENT_TIME = "timer"
-
+    # 创建引擎对象
     engine = AsyncEngine(work_core=200)
 
 
@@ -15,20 +15,14 @@ if __name__ == '__main__':
         await asyncio.sleep(1)
 
 
+    # 注册事件和对应的处理函数
     engine.register(EVENT_TIME, func)
+
+    # 开始处理
     engine.start()
     # 当前主线程调用
-    from time import time
 
-    start = time()
-    # 模拟推送1000000的事件
+    # 模拟推送100000的事件
     for _ in range(100000):
         # asyncio.run(engine.put(Event(EVENT_TIME, f"当前数据为{_}")))
         engine.put(Event(EVENT_TIME, f"当前数据为{_}"))
-
-    # 拿到任务结束的时间
-    while True:
-        if engine._queue.empty():
-            end = time()
-            break
-    print(f"耗时: {(end - start) * 1000} ms")
